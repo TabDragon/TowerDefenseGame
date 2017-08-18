3d46aaa2b536cbffbf3c4c0f9acd3828cb806d4b
TabDragon <31097001+TabDragon@users.noreply.github.com>
2017-08-18 22:46:41 +0200

    Add Tower selection

--- TowerDefenseGame/Assets/Scripts/BuildManager.cs
+++ TowerDefenseGame/Assets/Scripts/BuildManager.cs
@@ -4,6 +4,10 @@ public class BuildManager : MonoBehaviour
 {
     public static BuildManager instance;
     private TurretBlueprint turretToBuild;
+    private Node selectedNode;
+
+    public NodeUI nodeUI;
+
     public GameObject buildEffect;
     public bool CanBuild { get { return turretToBuild != null; } }
     public bool HasMoney { get { return PlayerStats.money >= turretToBuild.cost; } }
@@ -41,5 +45,26 @@ public class BuildManager : MonoBehaviour
     public void SelectTurretToBuild(TurretBlueprint turret)
     {
         turretToBuild = turret;
+        DeselectNode();
+    }
+
+    public void SelectNode(Node node)
+    {
+        if (selectedNode == node)
+        {
+            DeselectNode();
+            return;
+        }
+
+        selectedNode = node;
+        turretToBuild = null;
+
+        nodeUI.SetTarget(node);
+    }
+
+    public void DeselectNode()
+    {
+        selectedNode = null;
+        nodeUI.Hide();
     }
 }
--- TowerDefenseGame/Assets/Scripts/Node.cs
+++ TowerDefenseGame/Assets/Scripts/Node.cs
@@ -32,14 +32,14 @@ public class Node : MonoBehaviour
         if (EventSystem.current.IsPointerOverGameObject())
             return;
 
-        if (!buildManager.CanBuild)
-            return;
-
         if(turret != null)
         {
-            Debug.Log("Can't build there! - TODO: Display on screen.");
+            buildManager.SelectNode(this);
             return;
         }
+
+        if (!buildManager.CanBuild)
+            return;
 
         //Build a turret
         buildManager.BuildTurretOn(this);
