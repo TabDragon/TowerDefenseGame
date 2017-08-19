6774fe2ea9e85c1eed399ecaf86cad80e9a233b1
TabDragon <31097001+TabDragon@users.noreply.github.com>
2017-08-19 14:20:45 +0200

    Add health bar on enemies

--- TowerDefenseGame/Assets/Scripts/Enemy.cs
+++ TowerDefenseGame/Assets/Scripts/Enemy.cs
@@ -1,23 +1,31 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     [HideInInspector]
     public float speed;
 
-    public float startSpeed = 10f;
-    public float health     = 100f;
-    public int worth        = 50;
+    public float    startSpeed  = 10f;
+    public float    startHealth      = 100f;
+    private float   health;
+    public int      worth       = 50;
     public GameObject deathEffect;
+
+    [Header("Unity Stuff")]
+    public Image healthBar;
 
     private void Start()
     {
         speed = startSpeed;
+        health = startHealth;
     }
 
     public void TakeDamage(float amount)
     {
         health -= amount;
+
+        healthBar.fillAmount = health / startHealth;
 
         if (health <= 0)
         {
